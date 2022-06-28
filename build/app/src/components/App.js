921509049d9cc8d25e686724e896b101433aa58a
Patrick Schilf <pschilf@unb.ca>
2022-06-28 10:54:40 -0300

    IN-240 Toggle mobile navbar

--- build/app/src/components/App.js
+++ build/app/src/components/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import SiteList from './SiteList';
 
 function App(props) {
+  const [collapsed, setCollapsed] = useState(true)
+
+  const navbarToggle = () => {
+    setCollapsed((collapsed) => !collapsed)
+  }
+
   return (
     <React.Fragment>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -10,10 +16,10 @@ function App(props) {
           <a className="navbar-brand" href="#">
             <img src="/img/unb-libraries-red-black.png" alt="UNB Libraries Logo" />
           </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button className={`navbar-toggler${collapsed ? ' collapsed' : ''}`} type="button" onClick={navbarToggle} aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div id="navbarSupportedContent" className={`collapse navbar-collapse${!collapsed ? ' show' : ''}`}>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <NavLink to="/" className={({ isActive }) => `${isActive ? 'active ' : ''} nav-link`} aria-current="page">All</NavLink>
