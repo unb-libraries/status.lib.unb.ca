33ef6d3e3feda413a7b857d1c0c6ba5eb2c726ed
Patrick Schilf <pschilf@unb.ca>
2022-07-18 15:44:25 -0300

    IN-240 Create luxon time,interval,duration wrappers

--- build/app/src/helpers/time.js
+++ build/app/src/helpers/time.js
@@ -1,3 +1,72 @@
+import { DateTime as LxDateTime, Interval as LxInterval } from 'luxon'
+
+export class DateTime {
+  constructor(unixTimestamp) {
+    this._dt = LxDateTime.fromMillis(unixTimestamp)
+  }
+
+  static now = () => {
+    return new DateTime(LxDateTime.now().toMillis())
+  }
+
+  static fromTimestamp = (timestamp) => {
+    return new DateTime(timestamp)
+  }
+
+  toMilliseconds = () => {
+    return this._dt.toMillis()
+  }
+}
+
+export class Interval {
+  constructor(from, until) {
+    if (from.toMilliseconds() <= until.toMilliseconds()) {
+      this.interval = LxInterval.fromDateTimes(from._dt, until._dt)
+    }
+    else {
+      this.interval = LxInterval.fromDateTimes(until._dt, from._dt)
+    }
+  }
+
+  static fromDateTimes = (from, until) => {
+    return new Interval(from, until)
+  }
+
+  static untilNow = (from) => {
+    return this.fromDateTimes(from, DateTime.now())
+  }
+
+  duration = () => {
+    const d = this
+      .interval
+      .toDuration([
+        'days', 
+        'hours', 
+        'minutes', 
+        'seconds', 
+        'milliseconds'])
+      .toObject()
+    return new Duration(d.days, d.hours, d.minutes)
+  }
+}
+
+class Duration {
+  constructor(days, hours, minutes) {
+    this.days = Math.abs(days)
+    this.hours = Math.abs(hours)
+    this.minutes = Math.abs(minutes)
+  }
+
+  format = () => {
+    const units = {'day': this.days, 'hour': this.hours, 'minute': this.minutes}
+    const [unit, number] = Object.entries(units).find(([_, value]) => value > 0) || [undefined, 0]
+    if (unit) {
+      return '{} ago'.replace('{}', `${number} ${unit}${number !== 1 ? 's' : ''}`)
+    }
+    return 'Less than a minute ago'
+  }
+}
+
 export const setTime = (timestamp, hours, minutes, seconds, milliseconds = 0) => {
   const date = new Date(timestamp)
   date.setHours(hours)
