6609b5c69868d65506f2a5c7164af127c0b0b1eb
Patrick Schilf <pschilf@unb.ca>
2022-07-26 16:42:35 -0300

    IN-240 Site container padding

--- build/app/src/components/Site/PageList.js
+++ build/app/src/components/Site/PageList.js
@@ -1,14 +1,18 @@
-import Iconed, { Icons } from "../UI/Icon"
-import Page from "./Page"
+import Page from './Page'
 import classes from './PageList.module.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-const PageList = (props) => {
+const PageList = props => {
   return (
-    <ul>
+    <ul className={classes['page-list']}>
       {props.pages.map(page => (
         <li key={page.id} className={classes['page-list-item']}>
-          <Page id={page.id} title={page.title} href={`${props.siteUrl}${page.path}`} tests={page.tests} />
+          <Page
+            id={page.id}
+            title={page.title}
+            href={`${props.siteUrl}${page.path}`}
+            tests={page.tests}
+          />
         </li>
       ))}
     </ul>
