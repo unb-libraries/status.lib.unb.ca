edae9abda9db9f4a6f4f45cab60e56cc67ff1b63
Patrick Schilf <pschilf@unb.ca>
2022-10-07 12:23:20 -0300

    TESTING-3 Provide default filter,layout in config

--- build/app/src/hooks/useConfig.js
+++ build/app/src/hooks/useConfig.js
@@ -3,8 +3,8 @@ import { useSearchParams } from "react-router-dom"
 const useConfig = () => {
   const [params] = useSearchParams()
   const expandable = ['', '1', 'true'].includes(params.get('expandable'))
-  const filter = params.get('filter')
-  const layout = params.get('layout')
+  const filter = params.get('filter') || 'all'
+  const layout = params.get('layout') || 'full'
 
   return {
     expandable,
